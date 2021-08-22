7661a7ab4434b464676ae459efafa4548e9afcf0
Юрий <y.headman@ya.ru>
2021-08-22 17:20:32 +0300

    Добавляет отрисовку сообщения при отсутствии точек, обработчик нажатия ESC

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import {RenderPosition, render, replace} from './view/utils/render.js';
 import Menu from './view/menu.js';
 import RouteInfo from './view/route-info.js';
 import Filters from './view/filters.js';
-import PointsSort from './view/points-sort.js';
+import EventsSort from './view/events-sort.js';
 import EventList from './view/event-list.js';
 import Event from './view/event.js';
 import EditForm from './view/edit-form.js';
+import NoEvents from './view/no-events.js';
 
 const pointsCount = 10;
 const wayPoints = new Array(pointsCount).fill(null).map(generatePoints);
@@ -16,19 +17,27 @@ const tripControlsFilters = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
 const tripControlsNavigation = document.querySelector('.trip-controls__navigation');
 const events = new EventList();
+const noEventsMessage = new NoEvents();
+
+if (pointsCount > 0) {
+  render(tripMain, new RouteInfo(), RenderPosition.AFTERBEGIN);
+  render(tripEvents, new EventsSort(), RenderPosition.AFTERBEGIN);
+}
+if (pointsCount === 0) {
+  render(tripEvents, noEventsMessage, RenderPosition.BEFOREEND);
+}
 
 render(tripEvents, events, RenderPosition.BEFOREEND);
-render(tripMain, new RouteInfo(), RenderPosition.AFTERBEGIN);
 render(tripControlsNavigation, new Menu(), RenderPosition.BEFOREEND);
 render(tripControlsFilters, new Filters(), RenderPosition.BEFOREEND);
-render(tripEvents, new PointsSort(), RenderPosition.AFTERBEGIN);
+
 
 const renderEvent = (list, data) => {
   const eventPoint = new Event(data);
   const editForm = new EditForm(data);
   const editFormElement = editForm.getElement();
 
-  const replacePointToForm = () => {
+  const replaceEventToForm = () => {
     replace(eventPoint, editForm);
   };
 
@@ -36,17 +45,29 @@ const renderEvent = (list, data) => {
     replace(editForm, eventPoint);
   };
 
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceEventToForm();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   eventPoint.setEditClickHandler(() => {
     if (editFormElement) {
       render(events, editFormElement, RenderPosition.AFTERBEGIN);
     }
     replaceFormToEvent();
+    document.addEventListener('keydown', onEscKeyDown);
   });
+
   editForm.setformCloseHandler(() => {
-    replacePointToForm();
+    replaceEventToForm();
   });
+
   editForm.setformSubmitHandler(() => {
-    replacePointToForm();
+    replaceEventToForm();
+
   });
 
   render(events, eventPoint, RenderPosition.BEFOREEND);
